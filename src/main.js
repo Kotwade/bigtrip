160f82e9ab70170c011a2f47182b4b374650e139
Kotwade <kotwade@yandex.ru>
2022-07-05 14:25:19 +0500

    Добавление шаблонов

--- src/main.js
+++ src/main.js
@@ -1,39 +1,65 @@
-import {createMainMenuTemplate} from './view/main-menu.js';
-import {createTripInfoTemplate} from './view/trip-info.js';
-import {tripCostTemplate} from './view/trip-cost.js';
-import {createFilterTemplate} from './view/filter.js';
-import {createTripBordTemplate} from './view/trip-bord.js';
-import {createNewPointTemplate} from './view/new-point.js';
-import {createEditPointTemplate} from './view/edit-point.js';
-import {createPointTemplate} from './view/point.js';
+import MainMenuView from './view/main-menu.js';
+import FilterView from './view/filter.js';
+import TripInfoView from './view/trip-info.js';
+import TripCostView from './view/trip-cost.js';
+import TripSortView from './view/trip-sort.js';
+import PointListView from './view/point-list.js';
+import PointEditorView from './view/point-editor.js';
+import PointView from './view/point.js';
 
-const POINT_COUNT = 3;
+import { generatePointData } from './mock/point-data-generator.js';
+import { generateFilterData } from './mock/filter-data-generator.js';
+import { render, RenderPosition } from './utils.js';
+
+
+const POINT_COUNT = 20;
+
+const randomPointsData = new Array(POINT_COUNT).fill(null).map(generatePointData);
+const filterData = generateFilterData(randomPointsData);
+
+
 const siteBodyElement = document.querySelector('.page-body');
 
-const render = (container, template, position = 'beforeend') => {
-  container.insertAdjacentHTML(position, template);
+const menuElement = siteBodyElement.querySelector('.trip-controls__navigation');
+render(menuElement, new MainMenuView().getElement());
+
+const filterElement = siteBodyElement.querySelector('.trip-controls__filters');
+render(filterElement, new FilterView(filterData).getElement());
+
+const tripDetailsElement = siteBodyElement.querySelector('.trip-main');
+render(tripDetailsElement, new TripInfoView(randomPointsData).getElement(), RenderPosition.AFTERBEGIN);
+
+const tripInfoElement = tripDetailsElement.querySelector('.trip-info');
+render(tripInfoElement, new TripCostView(randomPointsData).getElement());
+
+const tripBoardElement = siteBodyElement.querySelector('.trip-events');
+render(tripBoardElement, new TripSortView().getElement());
+const pointListComponent = new PointListView();
+render(tripBoardElement, pointListComponent.getElement());
+
+
+const renderPoint = (pointListElement, pointData) => {
+  const pointComponent = new PointView(pointData);
+  const pointEditorComponent = new PointEditorView(pointData);
+
+  const changeViewToPoint = () => {
+    pointListElement.replaceChild(pointComponent.getElement(), pointEditorComponent.getElement());
+  };
+  const changeViewToEdit = () => {
+    pointListElement.replaceChild(pointEditorComponent.getElement(), pointComponent.getElement());
+  };
+
+  pointComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', changeViewToEdit);
+  pointEditorComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', changeViewToPoint);
+  pointEditorComponent.getElement().querySelector('.event--edit').addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    changeViewToPoint();
+  });
+
+  render(pointListElement, pointComponent.getElement());
 };
 
 
-const menuElement = siteBodyElement.querySelector('.trip-controls__navigation');
-render (menuElement, createMainMenuTemplate());
-
-const tripDetailsElement = siteBodyElement.querySelector('.trip-main');
-render(tripDetailsElement, createTripInfoTemplate(), 'afterbegin');
-
-const tripInfoElement = tripDetailsElement.querySelector('.trip-info');
-render(tripInfoElement, tripCostTemplate());
-
-const filterElement = siteBodyElement.querySelector('.trip-controls__filters');
-render(filterElement, createFilterTemplate());
-
-const tripBordElement = siteBodyElement.querySelector('.trip-events');
-render(tripBordElement, createTripBordTemplate());
-
-const eventListElement = tripBordElement.querySelector('.trip-events__list');
-render(eventListElement, createEditPointTemplate());
-render(eventListElement, createNewPointTemplate());
-
 for (let i = 0; i < POINT_COUNT; i++) {
-  render(eventListElement, createPointTemplate());
+  renderPoint(pointListComponent.getElement(), randomPointsData[i]);
 }
--- src/view/trip-cost.js
+++ src/view/trip-cost.js
@@ -1,5 +1,37 @@
-export const tripCostTemplate = () => {
+import { createElement } from './../utils.js';
+
+const getTotalCost = (pointData) => {
+  let totalCost = 0;
+  pointData.forEach(({ basePrice }) => totalCost += basePrice);
+  return totalCost;
+};
+
+
+const createTripCostTemplate = (pointData) => {
   return `<p class="trip-info__cost">
-    Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+    Total: &euro;&nbsp;<span class="trip-info__cost-value">${getTotalCost(pointData)}</span>
   </p>`;
 };
+
+
+export default class TripCost {
+  constructor(pointData) {
+    this._pointData = pointData;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTripCostTemplate(this._pointData);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
